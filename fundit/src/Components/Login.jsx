41380137d10348d0bd8a3cdd230d9e2f278f95ab
GauravZade2002 <aakashzade2002@gmail.com>
2023-12-25 22:43:32 +0530

    added tailwindCSS

---
+++ fundit/src/Components/Login.jsx
@@ -0,0 +1,13 @@
+
+
+function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hellow </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
