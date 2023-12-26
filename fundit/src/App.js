fd561b2103f8ded16ec2da381af5643801a7eef6
GauravZade2002 <aakashzade2002@gmail.com>
2023-12-26 22:23:08 +0530

    fixed TailwindCss

--- fundit/src/App.js
+++ fundit/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Login from './Components/Login';
 import Logout from './Components/Logout';
+import Landingpage from './Components/Landingpage';
 
 function App() {
   return (
     <>
+    <Landingpage/>
     <Login/>
    <Logout/>
     </>
