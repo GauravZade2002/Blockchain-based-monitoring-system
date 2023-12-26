c05d4b8c0c5b26c92320cb9189b7d759289648f6
GauravZade2002 <aakashzade2002@gmail.com>
2023-12-26 23:06:10 +0530

    nav added

--- fundit/src/App.js
+++ fundit/src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
     <>
     <Landingpage/>
-    <Login/>
-   <Logout/>
+    
     </>
     
   );
--- fundit/src/Components/Landingpage.js
+++ fundit/src/Components/Landingpage.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Login from './Login'
 
 const user = {
   name: 'Tom Cook',
@@ -189,12 +190,12 @@ export default function Landingpage() {
         </Disclosure>
 
         <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 bg-purple-100">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
           </div>
         </header>
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8"><Login/></div>
         </main>
       </div>
     </>
